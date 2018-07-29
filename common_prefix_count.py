085b5f5dd0d88b1cb25e4a1b6c118edb051c77c8
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-29 21:58:15 +0530

    Create common_prefix_count.py

---
+++ common_prefix_count.py
@@ -0,0 +1,8 @@
+nm=int(input())
+a=[]
+for i in range(0,nm):
+    a.append(input())
+list = [all(x[i] == x[i+1] for i in range(len(x)-1)) for x in zip(*a)]
+
+c=a[0][:list.index(0) if list.count(0) > 0 else len(list)]
+print(len(c))
