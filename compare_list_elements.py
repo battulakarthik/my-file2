0d175555b509deb8fc326d59b734c33bf0528af6
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-30 14:30:03 +0530

    Update compare_list_elements.py

--- compare_list_elements.py
+++ compare_list_elements.py
@@ -2,11 +2,13 @@ nm=int(input())
 a = [int(x) for x in input().split()]
 count=0
 for i in range(0,nm):
-    for j in range(i+1,nm):
+    j=i+1
+    if j+1<nm:
         if a[i]==a[j]:
+            count+=1
+        if count==1:
             k=a[i]
             print(k)
-            count=1
             break
-if count !=1 :
+else:
     print("unique")
