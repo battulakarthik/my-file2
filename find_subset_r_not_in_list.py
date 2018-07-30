f0a73e679ab68babaf4721d9245c7522a2109c85
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-30 15:42:23 +0530

    Create find_subset_r_not_in_list.py

---
+++ find_subset_r_not_in_list.py
@@ -0,0 +1,14 @@
+nm,k=input().split()
+nm=int(nm)
+k=int(k)
+count=0
+a = [int(x) for x in input().split()]
+b=[int(x) for x in input().split()]
+for i in range(0,k):
+    for j in range(0,nm):
+        if b[i]==a[j]:
+            count+=1
+if(count==len(b)):
+    print("yes")
+else:print("no")
+
