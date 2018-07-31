54cee50ac61523b02c3c11f420c1fc74eb7e2142
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-31 14:43:51 +0530

    Create removing_ovels_and_reverece.py

---
+++ removing_ovels_and_reverece.py
@@ -0,0 +1,13 @@
+d=int(input())
+nm=input()
+k=[]
+for i in range(0,d):
+    if nm[i]=="a" or nm[i]=="e" or nm[i]=="i" or nm[i]=="o" or nm[i]=="u":
+      gg=0
+    else:k.append(nm[i])
+k.reverse()
+for i in range(0,len(k)):
+    if i<len(k)-1:
+        m=' '
+    else:m=''
+    print(k[i],end=m)
