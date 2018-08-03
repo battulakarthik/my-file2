dcbe598d4c44d82bc198e6cc8f4f58359ce94b9e
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-03 14:15:13 +0530

    Update print odd numbers in given string.py

--- print odd numbers in given string.py
+++ print odd numbers in given string.py
@@ -1,5 +1,11 @@
 nm=[int(x) for x in input()]
+lst=[]
 for i in range(0,len(nm)):
     if nm[i]%2!=0:
-        print(nm[i],end=' ')
+        lst.append(nm[i])
+for i in range(0,len(lst)):
+    if i<len(lst)-1:
+        k=" "
+    else:k=''
+    print(lst[i],end=k)
 
