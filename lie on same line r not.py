a609c53fd1c23d1f961cdf37d6676c1e01fe1921
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-20 12:08:33 +0530

    Update lie on same line r not.py

--- lie on same line r not.py
+++ lie on same line r not.py
@@ -1,6 +1,6 @@
 n,m=map(int,input().split())
 k,b=map(int,input().split())
 h,j=map(int,input().split())
-if n==k==h or m==b==j:
+if n==k==h or m==b==j or (n==m and k==b and h==j):
     print("yes")
 else:print("no")
