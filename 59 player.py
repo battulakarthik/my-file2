580415f7717f620d718ed91532945072f3f6a2df
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-25 10:43:45 +0530

    Update 59 player.py

--- 59 player.py
+++ 59 player.py
@@ -1,7 +1,7 @@
 n=int(input())
-m=input().split()
+m=input()
 c=[]
-for i in range(n-1,-1,-1):
+for i in range(len(m)-1,-1,-1):
     if m[i]=='0':
         for j in range(i,-1,-1):
             if m[j]=='1':
