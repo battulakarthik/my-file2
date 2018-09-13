8be0122d2ee1a887fa748243fe61745f247cbb54
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-09-13 18:26:40 +0530

    Update 28 hunter.py

--- 28 hunter.py
+++ 28 hunter.py
@@ -1,15 +1,15 @@
 n=input()
 n=list(n)
-f=[]
+m=[]
 for i in range(0,len(n)):
     c=0
     for j in range(0,len(n)):
-        for k in range(0,len(f)):
-            if n[i]==f[k]:
+        for k in range(0,len(m)):
+            if n[i]==m[k]:
                 c+=1
         if i!=j and n[i]!=n[j]:
             c+=1
-            if c<=1:f.append(n[i])
-for i in range(0,len(f)):
-    print(f[i],end='')
+            if c<=1:m.append(n[i])
+for i in range(0,len(m)):
+    print(m[i],end='')
 
