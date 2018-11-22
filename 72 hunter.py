4626595d1aad5955c58e4fa133e4ab52938ada4b
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-11-22 12:24:35 +0530

    Update 72 hunter.py

--- 72 hunter.py
+++ 72 hunter.py
@@ -3,7 +3,10 @@ p=0
 for i in str:
     if p<len(str)-1:
         k=' '
-    else:k=''
+    else:
+        k=''
+        if i[len(i)-1]=='.':
+            i=i[:-1]
     if p%2==0:
         print(i[::-1],end=k)
     else:print(i,end=k)
